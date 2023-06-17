78916f1832b352262d6298d6a4f28ffd6aa21320
salvatoluice <salvatoluice5@gmail.com>
2023-06-17 16:09:50 +0300

    Make changes

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -17,9 +17,9 @@ const HeroSection = () => {
     <h1 class="text-6xl font-bold text-white mb-8">Empowering Growth and Success.</h1>
     <p class="text-lg text-white mb-8">Unlock Your Potential with Comprehensive Education and Training Solutions. With our dedicated team and high-quality programs, we empower individuals to excel in their endeavors and turn their dreams into reality.</p>
     <div class="flex items-center space-x-4 mb-8">
-      <a href="#" class="text-white hover:text-gray-200 transition duration-300">< FaFacebookF /></a>
-      <a href="#" class="text-white hover:text-gray-200 transition duration-300"><AiOutlineTwitter /></a>
-      <a href="#" class="text-white hover:text-gray-200 transition duration-300"><AiOutlineInstagram /></a>
+      <a target='_blank' href="https://facebook.com/" class="text-white hover:text-gray-200 transition duration-300">< FaFacebookF /></a>
+      <a target='_blank' href="https://twitter.com/" class="text-white hover:text-gray-200 transition duration-300"><AiOutlineTwitter /></a>
+      <a target='_blank' href="https://instagram.com/" class="text-white hover:text-gray-200 transition duration-300"><AiOutlineInstagram /></a>
     </div>
     <div class="flex space-x-4">
       <a href="#" class="inline-block bg-white text-black rounded-full px-4 py-2 hover:bg-gray-200 transition duration-300">Learn More</a>
